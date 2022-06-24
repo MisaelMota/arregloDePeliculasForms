b112e8b71911e3bbff087d797a3ab5e0df8c360a
MisaelMota <87102180+MisaelMota@users.noreply.github.com>
2022-06-23 22:49:01 -0400

    Add project files.

---
+++ arregloDePeliculasForms/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace arregloDePeliculasForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._titulo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._director = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._escritor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._genero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Clasificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this._Productora = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(124, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 77);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 29);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 139);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 29);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 195);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(124, 29);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 249);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(124, 29);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 301);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(124, 29);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 358);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(124, 29);
+            this.textBox7.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 40);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this._titulo,
+            this._director,
+            this._escritor,
+            this._genero,
+            this._Clasificacion,
+            this._Productora});
+            this.dataGridView1.Location = new System.Drawing.Point(163, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(645, 360);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // _titulo
+            // 
+            this._titulo.DataPropertyName = "titulo";
+            this._titulo.HeaderText = "Titulo";
+            this._titulo.Name = "_titulo";
+            // 
+            // _director
+            // 
+            this._director.DataPropertyName = "director";
+            this._director.HeaderText = "Director";
+            this._director.Name = "_director";
+            // 
+            // _escritor
+            // 
+            this._escritor.DataPropertyName = "escritor";
+            this._escritor.HeaderText = "Escritor";
+            this._escritor.Name = "_escritor";
+            // 
+            // _genero
+            // 
+            this._genero.DataPropertyName = "genero";
+            this._genero.HeaderText = "Genero";
+            this._genero.Name = "_genero";
+            // 
+            // _Clasificacion
+            // 
+            this._Clasificacion.DataPropertyName = "clasificacion";
+            this._Clasificacion.HeaderText = "Clasificacion";
+            this._Clasificacion.Name = "_Clasificacion";
+            // 
+            // _Productora
+            // 
+            this._Productora.DataPropertyName = "productora";
+            this._Productora.HeaderText = "Productora";
+            this._Productora.Name = "_Productora";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Titulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Director";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Escritor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Genero";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Duracion";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Clasificacion";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 342);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Productora";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 40);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Guardar en txt";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 467);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _titulo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _director;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _escritor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _genero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Clasificacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn _Productora;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ arregloDePeliculasForms/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arregloDePeliculasForms
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1[] MiPeliculas= new Form1[3];
+        int n = 0;
+
+        public string titulo { get; set; }
+        public string director { get; set; }
+        public string escritor { get; set; }
+        public string genero { get; set; }
+        public string duracion { get; set; }
+        public string clasificacion { get; set; }
+        public string productora { get; set; }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\LENOVO\OneDrive\Escritorio\Universidad\9no Semestre\Optimización de algoritmos\peliculas.txt", false))
+            {
+                for (int j = 0; j < MiPeliculas.Length; j++)
+                {
+                    writer.WriteLine(
+                        $"Pelicula numero {j + 1}:\n\n"
+                    + $"{MiPeliculas[j].titulo}\n"
+                    + $"{MiPeliculas[j].director}\n"
+                    + $"{MiPeliculas[j].escritor}\n"
+                    + $"{MiPeliculas[j].genero}\n"
+                    + $"{MiPeliculas[j].duracion}\n"
+                    + $"{MiPeliculas[j].clasificacion}\n"
+                    + $"{MiPeliculas[j].productora}\n"
+
+
+
+                        );
+                }
+
+                writer.Close();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            
+            
+                leerPeliculas(n);
+                n += 1;
+           
+                
+
+            
+
+        }
+
+        private void leerPeliculas(int i)
+        {
+            
+
+            
+            
+                Form1 form1 = new Form1();
+                form1.titulo = textBox1.Text;
+                form1.director = textBox2.Text;
+                form1.escritor = textBox3.Text;
+                form1.genero = textBox4.Text;
+                form1.duracion = textBox5.Text;
+                form1.clasificacion = textBox6.Text;
+                form1.productora = textBox7.Text;
+
+                MiPeliculas[i] = form1;
+
+                    dataGridView1.Rows.Add(
+                MiPeliculas[i].titulo,
+                MiPeliculas[i].director,
+                MiPeliculas[i].escritor,
+                MiPeliculas[i].genero,
+                MiPeliculas[i].duracion,
+                MiPeliculas[i].clasificacion,
+                MiPeliculas[i].productora);
+
+
+            
+
+           
+            
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
